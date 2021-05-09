96b77bae24cc4f015ab45852c9b2a91a5bce272e
elmehdi <elmehdiyessad@gmail.com>
2021-05-09 18:32:30 +0000

    Add FileManager folder to the project

--- client/src/app.js
+++ client/src/app.js
@@ -7,6 +7,7 @@ import LoginPage from './components/login/login';
 import NotFoundPage from './components/404';
 import RegisterPage from './components/signup/signup';
 import ProfilPage from './components/profil/profil';
+import HomePage from './components/FileManager/Home';
 
 export default function app() {
     return (
@@ -17,6 +18,7 @@ export default function app() {
                     <Route exact path="/index" component={MainPage} />
                     <Route exact path="/login" component={LoginPage} />
                     <Route exact path="/signup" component={RegisterPage} />
+                    <Route path="/home" component={HomePage}/>
                     <Route exact path="/profil" component={ProfilPage} />
                     <Route exact path="/404" component={NotFoundPage} />
                     <Redirect to="/404" />
