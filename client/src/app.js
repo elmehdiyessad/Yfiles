2649183195165a4aab6e9a22d3848364de49e593
elmehdi <elmehdiyessad@gmail.com>
2021-05-12 17:24:55 +0000

    Update app.js file

--- client/src/app.js
+++ client/src/app.js
@@ -7,6 +7,7 @@ import LoginPage from './components/login/login'
 import NotFoundPage from './components/404'
 import RegisterPage from './components/signup/signup'
 import HomePage from './components/fileManager/home'
+import ProfilPage from './components/fileManager/home'
 
 export default function app() {
     return (
@@ -18,6 +19,7 @@ export default function app() {
                     <Route exact path="/login" component={LoginPage} />
                     <Route exact path="/signup" component={RegisterPage} />
                     <Route exact path="/home" component={HomePage}/>
+                    <Route exact path="/profil" component={ProfilPage}/>
                     <Route exact path="/404" component={NotFoundPage} />
                     <Redirect to="/404" />
                 </Switch>
