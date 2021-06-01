4a3f442bd87bba812344b25605f219518e0c0ab3
elmehdi <elmehdiyessad@gmail.com>
2021-06-01 17:09:29 +0100

    make a home/footer responsive

--- client/src/components/index/footer.jsx
+++ client/src/components/index/footer.jsx
@@ -7,18 +7,18 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 export default function footer() {
     return (
         <>
-            <div id="footer" className="col-12">
-                <div className="footer-section col-sm-12 col-md-12 col-lg-4 col-xl-4">
+            <div id="footer" className="col-12 row">
+                <div className="footer-section col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
                     <img src="images/yfiles-logo.png" alt="yfiles-logo" width="300px" />
                 </div>
-                <div className="footer-section col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div className="footer-section col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
                     <h2>Links</h2>
                     <Link to="/" className="" role="">Security</Link>
                     <Link to="/about" className="" role="">Partners</Link>
                     <Link to="#services" className="" role="">Services</Link>
                     <Link to="/pricing" className="" role="">Pricing</Link>
                 </div>
-                <div className="footer-section col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div className="footer-section col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
                     <h2>Support</h2>
                     <Link to="/" className="" role=""><FontAwesomeIcon className="ml-2" icon={faEnvelope} /> Contact us</Link>
                     <Link to="/about" className="" role=""><FontAwesomeIcon className="ml-2" icon={faFacebook} /> Facebook</Link>
